368c5ddaa1e76664bfee4465c98502357f119bee
Ewelina Drozda <e.drozda@gmail.com>
2017-09-16 13:19:40 +0200

    Zad2 Tablice

---
+++ zad2/js/scripts.js
@@ -0,0 +1,10 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Magda'], //['Asia', 'Kasia', 'Ola', 'Marysia', 'Magda'],
+	maleNames = ['Piotrek', 'Marek', 'Tomek', 'Jarek'],
+	allNames = femaleNames.concat(maleNames),
+	newName = 'Marysia';
+	if (allNames.indexOf(newName) === -1) {
+		allNames.push('Marysia');
+	} else {
+		console.log('There is Marysia');
+	};
+	console.log(allNames);
