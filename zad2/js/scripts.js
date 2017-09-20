b39eb9b8460aa8a3e0108fdca84fadd3c1a05dc9
Ewelina Drozda <e.drozda@gmail.com>
2017-09-20 05:40:55 +0200

    zmienna

--- zad2/js/scripts.js
+++ zad2/js/scripts.js
@@ -1,9 +1,9 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Magda'], //['Asia', 'Kasia', 'Ola', 'Marysia', 'Magda'],
 	maleNames = ['Piotrek', 'Marek', 'Tomek', 'Jarek'],
 	allNames = femaleNames.concat(maleNames),
-	newName = 'Asia';
+	newName = prompt('Enter name');
 	if (allNames.indexOf(newName) === -1) {
-		allNames.push('Asia');
+		allNames.push(newName);
 	} else {
 		console.log('There is ' + newName);
 	};
